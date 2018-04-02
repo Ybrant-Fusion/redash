c0a99c6fc2419a9492effdde76365e149f061923
hagait <hagai.tob@gmail.com>
2018-04-02 14:54:03 +0300

    - Fixed deploy script typo - Changed to proper redash image in deployment file.

--- deploy_prod.bash
+++ deploy_prod.bash
@@ -5,7 +5,7 @@ echo "Pulling latest changes from git repository."
 git pull
 echo " deleting current deployment"
 docker-compose --project-name master -f docker-compose.production.yml down
-echo "removing redash/base image (causing errors while building docker image")
+echo "removing redash/base image (causing errors while building docker image"
 docker image remove redash/base
 echo "bulding docker image redash-brightcom-latest"
 docker build --no-cache -t redash-brightcom-latest .
